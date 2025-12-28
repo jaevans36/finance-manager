e066e08d74e22ec685e343283a1ec75b878fe0e4
Jay <jaevans36@gmail.com>
2025-12-28 21:02:23 +0000

    fix: add missing memo import to TaskItem component
    - Added memo to React imports in TaskItem.tsx
    - Resolves "memo is not defined" runtime error

--- apps/web/src/components/tasks/TaskItem.tsx
+++ apps/web/src/components/tasks/TaskItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styled from 'styled-components';
 import { Task } from '../../services/taskService';
 import { Card, Button, Text, TextSmall, Badge, Flex } from '../ui';
