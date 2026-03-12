905897fc039006a3b9a10458e004f11dae6ccaf3
Jay <jaevans36@gmail.com>
2026-03-12 17:37:27 +0000

    fix: skip database migration when using InMemory provider (integration test compatibility)

--- apps/finance-api/Program.cs
+++ apps/finance-api/Program.cs
@@ -184,7 +184,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<FinanceDbContext>();
-    db.Database.Migrate();
+    if (db.Database.IsRelational())
+        db.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline
